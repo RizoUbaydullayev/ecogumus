bab824c617b83721399787a8544de553e37433fb
RizoUbaydullayev <rizoubaydullayev17@gmail.com>
2023-08-07 18:41:19 +0500

    таблица

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 # Media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
